b07ed33ebeae193c65e9431e2326d4d03a5e2362
kf <kevtfung@gmail.com>
2022-12-07 16:19:44 -0800

    prices

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,18 +1,26 @@
 import { defineStore } from 'pinia'
 import json from '@/assets/FFTCGPrices.json'
+// import testCards from 'https://drive.google.com/file/d/1JEDb0trl-0A60GBWayHQY21EgKbrLE89/view?usp=sharing'
 
 // You can name the return value of `defineStore()` anything you want, 
 // but it's best to use the name of the store and surround it with `use` 
 // and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
 // the first argument is a unique id of the store across your application
-export const useCounterStore = defineStore('counter', {
+export const FFTCGCardPrices = defineStore('counter', {
     state: () => ({
         count: 0,
-        cardPrices: json
+        cardPrices: json,
+        cardLookup: ''
+        // CP2: testCards
       }),
 
     getters: {
         doubleCount: (state) => state.count * 2,
+        getCards (state) {
+            if(state.cardLookup.length > 0){
+                return state.cardPrices.filter(cn => cn.Name.toLocaleLowerCase().includes(state.cardLookup.toLocaleLowerCase()) || cn.Set.toLocaleLowerCase().includes(state.cardLookup.toLocaleLowerCase()))
+            }
+        }
     },
 
     actions: {
@@ -22,6 +30,9 @@ export const useCounterStore = defineStore('counter', {
         },
         randomizeCounter() {
           this.count = Math.round(100 * Math.random())
+        },
+        setCardLookup(str) {
+          this.cardLookup = str
         },
       },
         
--- src/views/TCGP_Prices.vue
+++ src/views/TCGP_Prices.vue
@@ -1,12 +1,32 @@
 <template>
   <NavBar></NavBar>
-  <v-btn href="https://drive.google.com/file/d/1JEDb0trl-0A60GBWayHQY21EgKbrLE89/view?pli=1">Excel</v-btn>
-  <v-text-field label="Card Name"></v-text-field>
-  <v-btn>Search</v-btn>
+  
+  <v-text-field label="Card Name" typ="text" v-model="cardFind" @change="setCardLookup"></v-text-field>
+  <a href="https://drive.google.com/file/d/1JEDb0trl-0A60GBWayHQY21EgKbrLE89/view?usp=sharing">Get All Cards in Excel</a>
+  
+  <li v-for="card in getCards" :key="card.set" style="display:flex; gap: 10px">
+    <a :href=card.Url_Details>{{card.Name.replaceAll("\"","")}}</a>
+    <p>{{card.Set.replaceAll("#","[")}}]</p>
+    <p><strong>{{card.Price}}</strong></p>
+  </li>
 </template>
 
 <script setup>
   import NavBar from '@/components/Nav-Bar'
+  import { FFTCGCardPrices } from '../stores/store'
+  import { storeToRefs } from 'pinia'
+  import { ref } from 'vue'
+
+
+  const cardFind = ref('')
+  const store = FFTCGCardPrices();
+  
+  store.setCardLookup(cardFind)
+
+  // storeToRefs lets todoList keep reactivity:
+  const { cardPrices, count, getCards} = storeToRefs(store)
+
+
 </script>
 
 <style>
